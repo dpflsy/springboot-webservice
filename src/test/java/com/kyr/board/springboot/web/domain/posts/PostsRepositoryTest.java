8c74066f902c0ba01b970962b29304ecaa1f63ee
예린 <dpflsy@gmail.com>
2020-04-14 01:46:42 +0900

    게시물 등록(post) 완성

---
+++ src/test/java/com/kyr/board/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.kyr.board.springboot.web.domain.posts;
+
+import com.kyr.board.springboot.domain.posts.Posts;
+
+
+import com.kyr.board.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After //JUnit 단위 테스트가 끝날 때마다 수행되는 메소드를 저장
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void open_saveBoard(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        //테이블 posts에 insert / update 쿼리를 실행
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("dpflsy@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll(); //테이블 posts의 모든 데이터를 조회해오는 메소드
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
---
+++ src/test/java/com/kyr/board/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+//HelloControllerTest를 롬복으로 대체
+package com.kyr.board.springboot.web.dto;
+
+import com.kyr.board.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void testLombok(){
+        //given
+        String name = "Test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name); //assertj라는 테스트 검증 라이브러리의 검증 메소드 - 메소드 체이닝 지원
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
