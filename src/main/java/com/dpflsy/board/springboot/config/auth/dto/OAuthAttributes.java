946b47c835b1c2b180b3be86d966c86387325274
예린 <dpflsy@gmail.com>
2020-04-26 01:43:02 +0900

    네이버 로그인 연동

--- src/main/java/com/dpflsy/board/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/dpflsy/board/springboot/config/auth/dto/OAuthAttributes.java
@@ -16,7 +16,7 @@ public class OAuthAttributes {
     private String picture;
 
 
-    //OAuth2User에서 반환한느 사용자 정보는 Map이기 때문에 하나하나 반환
+    //OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 하나하나 반환
     @Builder
     public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
         this.attributes = attributes;
@@ -27,7 +27,22 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
